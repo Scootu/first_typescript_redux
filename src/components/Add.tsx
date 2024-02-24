7090ae5bd44a048b5c3d3cc75d4b060e6bb42ebc
aneshamdaoui <bacetd2004@gmail.com>
2024-02-24 06:47:53 +0100

    fix default behaviour

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -7,7 +7,13 @@ export const Add = () => {
   const dispatch = useAppDispatch();
 
   return (
-    <form className=" bg-white p-2 border shadow m-2 ">
+    <form
+      className=" bg-white p-2 border shadow m-2 "
+      onSubmit={(e) => {
+        e.preventDefault();
+        dispatch(addPerson({ name: nameInput.current }));
+      }}
+    >
       <label className="border shadow-md rounded-md p-2 m-2">
         Person name:
       </label>
@@ -18,9 +24,7 @@ export const Add = () => {
         }}
       />
       <button
-        onClick={() => {
-          dispatch(addPerson({ name: nameInput.current }));
-        }}
+        type="submit"
         className="bg-violet-500 text-white rounded-md px-4 py-2 cursor-pointer hover:bg-violet-600 active:bg-violet-700"
       >
         Add
